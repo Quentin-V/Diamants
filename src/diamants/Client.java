ea1d48a017432b3782226ca0196b63f3fe95835d
jire18 <33605545+jire18@users.noreply.github.com>
2019-11-14 11:24:32 +0100

    Update Client.java
    Création client

--- src/diamants/Client.java
+++ src/diamants/Client.java
@@ -1,5 +1,25 @@
 package diamants;
 
-class Client {
 
+class Client {
+	private int				pochetTmp
+	private int				pochetSauvee;
+	private boolean			sortit;
+	private BufferedReader	in;
+	private PrintWritter	out;
+	
+	public Client(Socket s) {
+		this.pochetTmp		= 0;
+		this.pochetSauvee	= 0;
+		this.sortit			= false;
+		try {
+			this.out	= new PrintWriter(s.getOutputStream(), true);
+			this.in		= new BufferedReader(new InputStreamReader(s.getInputStream()));
+		}
+		catch(IOExeption ie) {
+			System.out.println(ie);
+		}
+	}
+	
+	
 }
